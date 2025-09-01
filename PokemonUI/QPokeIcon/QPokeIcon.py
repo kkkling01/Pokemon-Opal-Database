543e1258259ab98bd0d5f89a89efbb234f017048
kkkling01 <114850377+kkkling01@users.noreply.github.com>
2025-09-01 16:25:37 +0800

    Add files via upload

--- PokemonUI/QPokeIcon/QPokeIcon.py
+++ PokemonUI/QPokeIcon/QPokeIcon.py
@@ -1,1 +1,198 @@
+from typing import Union
+from PyQt6.QtCore import Qt, pyqtProperty, pyqtSignal, QSize
+from PyQt6.QtGui import (QPixmap, QPainter, QImage, QPainterPath,
+                         QImageReader, QMovie)
+from PyQt6.QtWidgets import QLabel, QWidget
+
+from qfluentwidgets.common.overload import singledispatchmethod
+
+
+class PokeIcon(QLabel):
+
+    clicked = pyqtSignal()
+
+    @singledispatchmethod
+    def __init__(self,
+                 image: QImage,
+                 parent: QWidget = None,
+                 ):
+        super().__init__(parent)
+        self.image = image
+        self.setBorderRadius(0, 0, 0, 0)
+        self._postInit()
+
+    @__init__.register
+    def _(self, image: str, parent=None):
+        self.__init__(parent)
+        self.setImage(image)
+
+    @__init__.register
+    def _(self, image: QImage, parent=None):
+        self.__init__(parent)
+        self.setImage(image)
+
+    @__init__.register
+    def _(self, image: QPixmap, parent=None):
+        self.__init__(parent)
+        self.setImage(image)
+
+    def _postInit(self):
+        pass
+
+    def _onFrameChanged(self, index: int):
+        self.image = self.movie().currentImage()
+        self.update()
+
+    def setBorderRadius(self, topLeft: int, topRight: int, bottomLeft: int, bottomRight: int):
+        """ set the border radius of image """
+        self._topLeftRadius = topLeft
+        self._topRightRadius = topRight
+        self._bottomLeftRadius = bottomLeft
+        self._bottomRightRadius = bottomRight
+        self.update()
+
+    def setImage(self, image: Union[str, QPixmap, QImage] = None):
+        """ set the image of label """
+        self.image = image or QImage()
+
+        if isinstance(image, str):
+            reader = QImageReader(image)
+            if reader.supportsAnimation():
+                self.setMovie(QMovie(image))
+            else:
+                self.image = reader.read()
+        elif isinstance(image, QPixmap):
+            self.image = image.toImage()
+
+        self.setFixedSize(self.image.size())
+        self.update()
+
+    def scaledToWidth(self, width: int):
+        if self.isNull():
+            return
+
+        h = int(width / self.image.width() * self.image.height())
+        self.setFixedSize(width, h)
+
+        if self.movie():
+            self.movie().setScaledSize(QSize(width, h))
+
+    def scaledToHeight(self, height: int):
+        if self.isNull():
+            return
+
+        w = int(height / self.image.height() * self.image.width())
+        self.setFixedSize(w, height)
+
+        if self.movie():
+            self.movie().setScaledSize(QSize(w, height))
+
+    def setScaledSize(self, size: QSize):
+        if self.isNull():
+            return
+
+        self.setFixedSize(size)
+
+        if self.movie():
+            self.movie().setScaledSize(size)
+
+    def isNull(self):
+        return self.image.isNull()
+
+    def mouseReleaseEvent(self, e):
+        super().mouseReleaseEvent(e)
+        self.clicked.emit()
+
+    def setPixmap(self, pixmap: QPixmap):
+        self.setImage(pixmap)
+
+    def pixmap(self) -> QPixmap:
+        return QPixmap.fromImage(self.image)
+
+    def setMovie(self, movie: QMovie):
+        super().setMovie(movie)
+        self.movie().start()
+        self.image = self.movie().currentImage()
+        self.movie().frameChanged.connect(self._onFrameChanged)
+
+    def paintEvent(self, e):
+        if self.isNull():
+            return
+
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.RenderHint.Antialiasing)
+
+        path = QPainterPath()
+        w, h = self.width(), self.height()
+
+        # top line
+        path.moveTo(self.topLeftRadius, 0)
+        path.lineTo(w - self.topRightRadius, 0)
+
+        # top right arc
+        d = self.topRightRadius * 2
+        path.arcTo(w - d, 0, d, d, 90, -90)
+
+        # right line
+        path.lineTo(w, h - self.bottomRightRadius)
+
+        # bottom right arc
+        d = self.bottomRightRadius * 2
+        path.arcTo(w - d, h - d, d, d, 0, -90)
+
+        # bottom line
+        path.lineTo(self.bottomLeftRadius, h)
+
+        # bottom left arc
+        d = self.bottomLeftRadius * 2
+        path.arcTo(0, h - d, d, d, -90, -90)
+
+        # left line
+        path.lineTo(0, self.topLeftRadius)
+
+        # top left arc
+        d = self.topLeftRadius * 2
+        path.arcTo(0, 0, d, d, -180, -90)
+
+        # draw image
+        image = self.image.scaled(
+            self.size()*self.devicePixelRatioF(), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
+
+        painter.setPen(Qt.PenStyle.NoPen)
+        painter.setClipPath(path)
+        painter.drawImage(self.rect(), image)
+
+    @pyqtProperty(int)
+    def topLeftRadius(self):
+        return self._topLeftRadius
+
+    @topLeftRadius.setter
+    def topLeftRadius(self, radius: int):
+        self.setBorderRadius(radius, self.topRightRadius, self.bottomLeftRadius, self.bottomRightRadius)
+
+    @pyqtProperty(int)
+    def topRightRadius(self):
+        return self._topRightRadius
+
+    @topRightRadius.setter
+    def topRightRadius(self, radius: int):
+        self.setBorderRadius(self.topLeftRadius, radius, self.bottomLeftRadius, self.bottomRightRadius)
+
+    @pyqtProperty(int)
+    def bottomLeftRadius(self):
+        return self._bottomLeftRadius
+
+    @bottomLeftRadius.setter
+    def bottomLeftRadius(self, radius: int):
+        self.setBorderRadius(self.topLeftRadius, self.topRightRadius, radius, self.bottomRightRadius)
+
+    @pyqtProperty(int)
+    def bottomRightRadius(self):
+        return self._bottomRightRadius
+
+    @bottomRightRadius.setter
+    def bottomRightRadius(self, radius: int):
+        self.setBorderRadius(
+            self.topLeftRadius, self.topRightRadius, self.bottomLeftRadius, radius)
+
 
